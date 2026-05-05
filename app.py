ca80453996006003de03f22f4c5c9bb88206280d
ektha mallya <mallyaektha@gmail.com>
2026-05-05 20:48:15 +0530

    debugging

--- app.py
+++ app.py
@@ -79,7 +79,7 @@ uploaded_file = st.file_uploader(
 )
 
 if uploaded_file and not st.session_state.file_loaded:
-    print(uploaded_file)
+    print("---------------------------------FILE UIPLOADED---------------------",uploaded_file)
     with st.spinner("Chunking → Embedding → Storing in FAISS…"):
         suffix = "." + uploaded_file.name.split(".")[-1]
         with tempfile.NamedTemporaryFile(delete=False, suffix=suffix) as tmp:
@@ -87,7 +87,7 @@ if uploaded_file and not st.session_state.file_loaded:
             tmp_path = tmp.name
         try:
             data = load_data(tmp_path)
-            print(data)
+            print("-------------------------------DATA----------------------",data)
             st.session_state.agent.load(data)
             st.session_state.file_loaded = True
             st.session_state.messages = []
